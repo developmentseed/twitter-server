b2d2957c38f569dfd2092581c15b0a75c56c953f
Derek Lieu <derek@developmentseed.org>
2014-03-31 17:39:16 -0400

    Included sample S3 & Twitter credentials

---
+++ credentials/sample_twitter_credential.js
@@ -0,0 +1,6 @@
+module.exports = {
+	consumer_key: "YOURCONSUMERKEY",
+	consumer_secret: "YOURCONSUMERSECRET",
+	access_token_key: "YOURACCESSTOKENKEY",
+	access_token_secret: "YOURACCESSTOKENSECRET"
+}
